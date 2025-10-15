eb9d6463a36e5fc3ebc39a0210439382dc5a4110
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-15 11:59:25 +0100

    feat(index) add addition function with default parameters and example usage

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function addition(a = 0, b = 1) {
+  return a + b;
+}
+
+var product = addition(5, 10);
+var product2 = addition(6, 7);
+var product3 = addition(1);
+
+console.log("product :", product);
+console.log("product2 :", product2);
+console.log("product3 :", product3);
