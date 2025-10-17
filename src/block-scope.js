160a0b5c3fb19891c1fea99c890e7f2806775a00
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-17 12:05:28 +0100

    feat(scope files) add new function scope and block scope

---
+++ src/block-scope.js
@@ -0,0 +1,19 @@
+// for (var a = 1; a <= 5; a++) {
+//   console.log("a: ", a);
+// }
+
+// console.log("a: ", a);
+
+const a = 1;
+let b;
+
+if (a === 1) {
+  console.log("correct");
+  b = 2;
+
+  console.log("b :", b);
+} else {
+  console.log("incorrect");
+}
+
+console.log("outside if statement b:", b);
---
+++ src/function-scope.js
@@ -0,0 +1,10 @@
+let a = 0;
+
+function reassignA() {
+  a = 5;
+}
+
+reassignA();
+
+console.log("a :", a);
+//console.log("b :", b);
