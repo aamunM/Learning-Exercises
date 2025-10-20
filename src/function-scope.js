e41bc9c2cd227879667aea14d674f5f953701bb6
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-20 12:49:52 +0100

    docs(function-scope) add comments for improved documentation

--- src/function-scope.js
+++ src/function-scope.js
@@ -1,12 +1,29 @@
+//global scope
 let a = 0;
 let b = 0;
+let bWasSet = false;
 
 function reassignA() {
+  //function scope
   a = 5;
-  b = 10;
+}
+
+function reassignB() {
+  //function scope
+  if (b !== 10) {
+    //block scope
+    b = 10;
+    if (bWasSet === false) {
+      //block scope
+      bWasSet = true;
+    }
+  }
 }
 
 reassignA();
+reassignB();
 
 console.log("a :", a);
 console.log("b :", b);
+console.log("bWasSet: ", bWasSet);
+console.log("const ", random);
