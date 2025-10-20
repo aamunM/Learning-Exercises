fcfed6f9b258cbf8b1e417559cfb31fe2394e3cc
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-20 12:49:26 +0100

    docs(block-scope) add comments for improved documentation

--- src/block-scope.js
+++ src/block-scope.js
@@ -4,15 +4,18 @@
 
 // console.log("a: ", a);
 
+//global scope
 const a = 1;
 let b;
 
 if (a === 1) {
+  //block scope
   console.log("correct");
   b = 2;
 
   console.log("b :", b);
 } else {
+  //global scope
   console.log("incorrect");
 }
 
