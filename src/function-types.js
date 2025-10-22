69969593782391ab0c958888d3821b55a4781260
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-22 11:36:38 +0100

    feat(function-types) add arrow function examples

--- src/function-types.js
+++ src/function-types.js
@@ -5,10 +5,20 @@ function greet(name) {
   return `hello ${name}`;
 }
 
+console.log("With String Interpolation: ");
 const greetAamun = greet("Aamun");
 console.log(greetAamun);
 
 const greetAlex = greet("Alex");
 console.log(greetAlex);
+
+//arrow functions
+const greetArrowFunction = (name) => {
+  return `hello ${name}`;
+};
+
+console.log("\nWith Arrow Functions:");
+console.log(greetArrowFunction("Aamun"));
+console.log(greetArrowFunction("Alex"));
 
 //arrow functions, anonymous functions and this
