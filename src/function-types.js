0fe0012a6d97dbcd43ad7e3f4fdcd69a668dff2d
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-22 11:04:54 +0100

    feat(function-types)add examples of string interpolation functions

---
+++ src/function-types.js
@@ -0,0 +1,14 @@
+// traditional function declaration
+
+function greet(name) {
+  //string interpolation
+  return `hello ${name}`;
+}
+
+const greetAamun = greet("Aamun");
+console.log(greetAamun);
+
+const greetAlex = greet("Alex");
+console.log(greetAlex);
+
+//arrow functions, anonymous functions and this
