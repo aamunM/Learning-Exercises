c80872818d7d087d6c1c94e52802cfda8c0f6a3f
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-23 11:15:38 +0100

    chore(context)add comments to explain significance of arrow functions

--- src/context.js
+++ src/context.js
@@ -2,19 +2,19 @@ const user = {
   name: "Aamun",
   hobbies: ["Coding", "Running", "Football"],
 
+  //without arrow function, cannot scope 'this.name' as it is outside the scope
   showHobbies: function () {
-    this.name = "Alex";
     this.hobbies.forEach(function (hobby) {
-      this.name = "Joe";
       console.log(`${this.name} likes ${hobby}`);
     });
   },
 
+  //arrow function, can scope 'this.name' with an arrow function
   showHobbiesArrow: function () {
     this.hobbies.forEach((hobby) => console.log(`${this.name} likes ${hobby}`));
   },
 };
 //console.log(user.name);
 //console.log(user.hobbies);
-//console.log(user.showHobbies());
+console.log(user.showHobbies());
 console.log(user.showHobbiesArrow());
