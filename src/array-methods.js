57806ab91297258b89d91ab6c5c21800e7ef73dc
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-10-31 12:01:46 +0000

    feat(array-methods): add examples of arrays with different property types

---
+++ src/array-methods.js
@@ -0,0 +1,39 @@
+const things = [
+  {
+    name: "aamun",
+    age: 30,
+  },
+  {
+    name: "Alex",
+    age: 25,
+  },
+  50,
+  "random",
+  false,
+  [5, 7, false, "string"],
+];
+
+const person = {
+  name: "Jack",
+  age: 50,
+  isMarried: false,
+  neighbour: {
+    name: "john",
+    age: 60,
+  },
+  children: [
+    {
+      name: "Emma",
+      age: 4,
+    },
+    {
+      name: "Mark",
+      age: 6,
+    },
+  ],
+};
+
+//console.log(things.length);
+console.log(things[things.length - 1]);
+
+//map, filter, reduce
