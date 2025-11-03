2bce0466a338a4e401eb45d27bb298c027dcc2a1
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-11-03 11:30:36 +0000

    docs(array-methods): add comments to explain array methods

--- src/array-methods.js
+++ src/array-methods.js
@@ -1,4 +1,7 @@
+//Array of multiple objects, parameters etc.
+
 const things = [
+  //multiple objects in an array
   {
     name: "aamun",
     age: 30,
@@ -7,12 +10,15 @@ const things = [
     name: "Alex",
     age: 25,
   },
+  //num in an array
   50,
   "random",
   false,
+  //another Array in the array
   [5, 7, false, "string"],
 ];
 
+//Object with multiple objects or arrays inside with other parameters
 const person = {
   name: "Jack",
   age: 50,
@@ -34,6 +40,8 @@ const person = {
 };
 
 //console.log(things.length);
+
+//method to log the last parameter in the array
 console.log(things[things.length - 1]);
 
 //map, filter, reduce
