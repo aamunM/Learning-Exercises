8cf661142af8463a01d620f3ac2f6a7f98767a7b
Aamun Malhotra <122031259+aamunM@users.noreply.github.com>
2025-11-04 12:03:44 +0000

    feat(array-methods): add examples and comments for map and filter methods

--- src/array-methods.js
+++ src/array-methods.js
@@ -42,6 +42,62 @@ const person = {
 //console.log(things.length);
 
 //method to log the last parameter in the array
-console.log(things[things.length - 1]);
+//console.log(things[things.length - 1]);
 
 //map, filter, reduce
+
+//map
+const people = [
+  {
+    name: "Aamun",
+    age: 30,
+    occupation: "QA",
+    hobbies: ["coding", "running", "gaming"],
+  },
+  {
+    name: "John",
+    age: 40,
+    occupation: "Electrician",
+    hobbies: ["woodwork", "gym", "football"],
+  },
+  {
+    name: "James",
+    age: 21,
+    occupation: "Student",
+    hobbies: ["rugby", "space", "youtube"],
+  },
+];
+
+//new variable "names", map method used on array above, function to return only names
+const names = people.map((person) => person.name);
+
+const allHobbies = people.map((person) => person.hobbies);
+
+const secondHobby = people.map((person) => person.hobbies[1]);
+
+const returnHobby = (index) => people.map((person) => person.hobbies[index]);
+
+//console.log(names);
+//console.log(returnHobby(1));
+
+//filter
+const ageAbove25 = people.filter((person) => person.age > 25);
+
+const areQA = people.filter((person) => person.occupation === "QA");
+
+const filterByOccupation = (occupation) =>
+  people.filter((person) => person.occupation === occupation);
+
+const filterByName = (name) => people.filter((person) => person.name === name);
+
+//console.log(areQA);
+
+//console.log(filterByName("John"));
+
+const numbers = [1, 2, 3, 4, 5];
+
+const doubleNumbers = numbers.map((num) => num * 2);
+const cubedNumbers = numbers.map((num) => Math.pow(num, 3));
+const powerOfNumber = (power) => numbers.map((num) => Math.pow(num, power));
+
+console.log(powerOfNumber(3));
